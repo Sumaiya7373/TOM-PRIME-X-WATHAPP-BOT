84dcb5ffe99a59e943960926365a115661b5811b
Sumaiya7373 <ranashahin11102005@gmail.com>
2026-07-10 03:38:15 +0600

    Update owner reply message with owner's name

--- commands/general/owner.js
+++ commands/general/owner.js
@@ -40,7 +40,7 @@ END:VCARD
                 }
             });
 
-            await extra.reply('👑 Here is the contact of my *Owner*.');
+            await extra.reply('👑𝐓ʜᴇ 𝐊ɪɴɢ ᴏғ 𝐒ʜᴀʜɪɴ 𝐑ᴀɴᴀ 👑*Owner*.');
 
         } catch (error) {
             console.error('Owner command error:', error);
